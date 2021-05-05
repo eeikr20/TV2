98483013cf624db2f3c65c24ce85997106b82a3b
ander <patand1993@gmail.com>
2021-05-05 10:00:08 +0200

    sat main path til HomeScreen

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,16 +2,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/HomeScreen.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("Home Screen");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
