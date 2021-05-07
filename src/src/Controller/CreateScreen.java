2d25c123fad96a9b23de3eb8831784e8a0553f51
Dinh Phu Luu <diluu20@student.sdu.dk>
2021-05-07 12:46:53 +0200

    GUI 1.12

--- src/src/Controller/CreateScreen.java
+++ src/src/Controller/CreateScreen.java
@@ -1,4 +1,16 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class CreateScreen {
+
+    @FXML
+    void exit(MouseEvent event) throws IOException {
+
+        MainFX.setScene(getClass().getResource("/FXML/HomeScreen.fxml"), "Home Screen" );
+
+    }
 }
--- src/src/Controller/LogInScreen.java
+++ src/src/Controller/LogInScreen.java
@@ -1,16 +1,23 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
+
 public class LogInScreen {
+    @FXML
+    private TextField fiUsername;
+    @FXML
+    private PasswordField fiPassword;
 
     @FXML
     void exit(MouseEvent event) throws IOException {
 
-        MainFX.setScene(getClass().getResource("/FXML/LogInScreen.fxml"), "Log In" );
+        MainFX.setScene(getClass().getResource("/FXML/HomeScreen.fxml"), "Home Screen" );
 
     }
     @FXML
@@ -21,7 +28,13 @@ public class LogInScreen {
     }
     @FXML
     void confirmLogIn(MouseEvent event) throws IOException {
-
+        /*
+        if (String fiUsername !& fiPassword == )
+        System.out.println("Wrong Username and/or Password");
+        else
+         */
+            // Set current user to account the correct crudential is ergo. Admin or producer or account
+        // perhaps a popup screen to display succesfully on login
         MainFX.setScene(getClass().getResource("/FXML/HomeScreen.fxml"), "Home Screen" );
 
     }
