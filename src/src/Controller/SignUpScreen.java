1936fb027b5e123341578ebd06daab9d1f7c2d2f
Mikkel Duus Normann Hansen <mikkelduus2000@gmail.com>
2021-05-07 12:31:50 +0200

    Added SignUpScreen

---
+++ src/src/Controller/SignUpScreen.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignUpScreen {
+    String userName;
+    String email;
+    String password;
+    String confirmationPassword;
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+    @FXML
+    void passwordVisibility(ActionEvent event) {
+
+    }
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+}
