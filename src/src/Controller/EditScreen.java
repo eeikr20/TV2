2351a5a1054230e8f574cc82c9335125efef0299
Dinh Phu Luu <diluu20@student.sdu.dk>
2021-05-24 16:45:03 +0200

    EditScreen

---
+++ src/src/Controller/EditScreen.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+
+
+public class EditScreen {
+    @FXML
+    private Button btAdd;
+    @FXML
+    private Label idName;
+    @FXML
+    private Button btVerifyProgram;
+    @FXML
+    private Button btVerifyCast;
+    @FXML
+    private Button btVerifyCredit;
+    @FXML
+    private Button btAddCredit;
+    @FXML
+    private Button btRemoveCredit;
+    @FXML
+    private ListView idCreditList;
+    @FXML
+    private ListView idCastlist;
+    @FXML
+    private TextField fiCastName;
+
+
+    @FXML
+    public void addCredit(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void searchString(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void removeCredit(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void addNewCast(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void verifyProgram(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void verifyCast(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void verifyCredit(MouseEvent event) {
+
+    }
+
+
+}
